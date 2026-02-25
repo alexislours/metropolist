f94bd9a98b5831a211bf8a23bd6665c5339d9f8a
Alexis LOURS <alexislours@protonmail.com>
2026-02-25 21:06:19 +0100

    feat: quick action

--- metropolist/metropolistApp.swift
+++ metropolist/metropolistApp.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 @main
 struct MetropolistApp: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
     @State private var dataStore = DataStore()
 
     #if DEBUG
@@ -11,6 +12,14 @@ struct MetropolistApp: App {
     var body: some Scene {
         WindowGroup {
             MainTabView()
+                .task {
+                    registerQuickActions()
+                    handleQuickAction(appDelegate.pendingQuickActionType)
+                    appDelegate.pendingQuickActionType = nil
+                }
+                .onReceive(NotificationCenter.default.publisher(for: .quickActionTriggered)) { notification in
+                    handleQuickAction(notification.object as? String)
+                }
             #if DEBUG
                 .task {
                     if isScreenshotMode {
@@ -20,5 +29,53 @@ struct MetropolistApp: App {
             #endif
         }
         .environment(dataStore)
+    }
+
+    private func registerQuickActions() {
+        UIApplication.shared.shortcutItems = [
+            UIApplicationShortcutItem(
+                type: "com.alexislours.metropolist.startTravel",
+                localizedTitle: String(localized: "Start travel"),
+                localizedSubtitle: nil,
+                icon: UIApplicationShortcutIcon(systemImageName: "play.fill")
+            ),
+        ]
+    }
+
+    private func handleQuickAction(_ type: String?) {
+        guard type == "com.alexislours.metropolist.startTravel" else { return }
+        dataStore.travelFlowPrefill = TravelFlowPrefill()
+    }
+}
+
+extension Notification.Name {
+    static let quickActionTriggered = Notification.Name("com.alexislours.metropolist.quickAction")
+}
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    var pendingQuickActionType: String?
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        if let shortcutItem = options.shortcutItem {
+            pendingQuickActionType = shortcutItem.type
+        }
+        let config = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        config.delegateClass = SceneDelegate.self
+        return config
+    }
+}
+
+final class SceneDelegate: NSObject, UIWindowSceneDelegate {
+    func windowScene(
+        _ windowScene: UIWindowScene,
+        performActionFor shortcutItem: UIApplicationShortcutItem,
+        completionHandler: @escaping @Sendable (Bool) -> Void
+    ) {
+        NotificationCenter.default.post(name: .quickActionTriggered, object: shortcutItem.type)
+        completionHandler(true)
     }
 }
