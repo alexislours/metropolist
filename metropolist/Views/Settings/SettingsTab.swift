2430eda95d114209f8c9a5202776d1d02c6c303b
Alexis LOURS <alexislours@protonmail.com>
2026-02-25 21:57:25 +0100

    fix: linter

--- metropolist/Views/Settings/SettingsTab.swift
+++ metropolist/Views/Settings/SettingsTab.swift
@@ -382,20 +382,3 @@ struct SettingsTab: View {
         }
     }
 }
-
-// MARK: - Supporting Types
-
-struct TransitStats {
-    let totalLines: Int
-    let totalStations: Int
-    let totalBranches: Int
-    let linesByMode: [(mode: TransitMode, count: Int)]
-    let generatedAt: String?
-    let databaseSize: String?
-}
-
-struct ImportAlert: Identifiable {
-    let id = UUID()
-    let title: String
-    let message: String
-}
