4ddc33cb7152db94fa7043788888413f5d7cdd90
Alexis LOURS <alexislours@protonmail.com>
2026-02-26 02:22:06 +0100

    fix: set en_US_POSIX locale on dayFormatter to ensure Gregorian calendar output

--- metropolist/Gamification/GamificationEngine.swift
+++ metropolist/Gamification/GamificationEngine.swift
@@ -12,6 +12,7 @@ enum GamificationEngine {
 
     private static let dayFormatter: DateFormatter = {
         let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
         formatter.dateFormat = "yyyy-MM-dd"
         return formatter
     }()
